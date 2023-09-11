4f4ca6f2fa014bd8b09f16b2749f8ad5e2d8959e
Sonyx17 <villafsonia@gmail.com>
2023-09-11 14:55:24 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const path = require ("path");
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.listen(3000, ()=> {
+console.log("Servidor Corriendo")
+});
+
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname, './views/home.html'));
+})
+app.get('/register', (req,res) =>{
+    res.sendFile(path.join(__dirname,'./views/register.html'));
+})
+    
