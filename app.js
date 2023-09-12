804f68f4d66a0417d2151b881a3fed6af1fdeea3
Sonyx17 <villafsonia@gmail.com>
2023-09-11 23:24:29 -0300

    cambiando rutas

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require ("path");
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, './public');
+const publicPath = path.resolve(__dirname, 'public');
 app.use(express.static(publicPath));
 
 app.listen(3000, ()=> {
@@ -11,12 +11,12 @@ console.log("Servidor Corriendo")
 });
 
 app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, './views/home.html'));
+res.sendFile(path.join(__dirname, 'views', 'home.html'));
 })
 app.get('/register', (req,res) =>{
-    res.sendFile(path.join(__dirname,'./views/register.html'));
+    res.sendFile(path.join(__dirname,'views', 'register.html'));
 })
 app.get('/login', (req,res) =>{
-    res.sendFile(path.join(__dirname,'./views/login.html'));
+    res.sendFile(path.join(__dirname,'views', 'login.html'));
 })
     
